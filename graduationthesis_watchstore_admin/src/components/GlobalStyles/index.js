6217935afa3b2a605e51fba1ea67df665b0c5acb
Plun <phuchoangxyz@gmail.com>
2023-02-03 11:00:52 +0700

    base admin

---
+++ graduationthesis_watchstore_admin/src/components/GlobalStyles/index.js
@@ -0,0 +1,9 @@
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'swiper/css/bundle';
+import './GlobalStyles.scss';
+
+function GlobalStyles({ children }) {
+    return children;
+}
+
+export default GlobalStyles;
